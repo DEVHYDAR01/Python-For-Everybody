1a134b84b1623115a4538371753bdad376a2451a
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-13 00:07:58 +0100

    This commit will push list

--- lists.py
+++ lists.py
@@ -1,2 +1,8 @@
-names = ["hydar", "nana", "bob", "walid"]
-print("{}".format(names))
+import pandas
+names = []
+i = 0
+while i < 3:
+    names = input("enter names\n")
+    print("this is your names")
+    print(names)
+    i = i + 1
